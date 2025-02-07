01a839ab938d4501a5c63d275a2e3a93d729c6a3
VMahluza <mahluzavictor@gmail.com>
2025-02-07 16:12:57 +0200

    Spring Init

---
+++ src/main/java/com/minimal_ecommerce/minimal_ecommerce/MinimalEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.minimal_ecommerce.minimal_ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinimalEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinimalEcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/minimal_ecommerce/minimal_ecommerce/MinimalEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minimal_ecommerce.minimal_ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinimalEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
