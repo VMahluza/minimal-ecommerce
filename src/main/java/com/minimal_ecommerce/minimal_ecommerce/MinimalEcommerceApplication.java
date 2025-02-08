0167030f99f1cb982db8f8a024bd8f68d127a61c
VMahluza <mahluzavictor@gmail.com>
2025-02-08 08:52:07 +0200

    User & Category Entity

--- src/main/java/com/minimal_ecommerce/minimal_ecommerce/MinimalEcommerceApplication.java
+++ src/main/java/com/minimal_ecommerce/minimal_ecommerce/MinimalEcommerceApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MinimalEcommerceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MinimalEcommerceApplication.class, args);
 	}
-
 }
